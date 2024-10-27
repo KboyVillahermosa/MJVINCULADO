ecb9ed198590e599743eab08a4fa7fbf34390b27
kboy25 <fvillahermosa_ccs@uspf.edu.ph>
2024-10-27 11:01:24 +0800

    added exp

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,52 +1,67 @@
 import React from 'react'
-import { Button, Timeline } from "flowbite-react";
-import { HiArrowNarrowRight, HiCalendar } from "react-icons/hi";
+import './Experience.css'
+import {  Timeline } from "flowbite-react";
+import { HiCalendar } from "react-icons/hi";
+import Lottie from 'lottie-react'
+import animationData from './exp.json'
 
 
 const Experience:React.FC = () => {
   return (
-    <> <section className='dark:bg-gray-800'>
-       
+    <> 
+    <section className='dark:bg-gray-800'>
+       <div className="exp">
+        <h1 className='dark:text-white uppercase'>Project Experience</h1>
+       </div>
     <div className="timeline-header">
         <div className="timeline-content">
         <Timeline>
       <Timeline.Item>
         <Timeline.Point icon={HiCalendar} />
         <Timeline.Content>
-          <Timeline.Time>February 2022</Timeline.Time>
-          <Timeline.Title>Application UI code in Tailwind CSS</Timeline.Title>
+          <Timeline.Time>June – July 2024</Timeline.Time>
+          <Timeline.Title>Leo Revita Salon - Trade Center Branch</Timeline.Title>
           <Timeline.Body>
-            Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order
-            E-commerce & Marketing pages.
+           <p className='dark:text-white'>Assisted with various salon operations and customer service.</p>
+           <p className='dark:text-white'>Provided technical support and contributed to daily administrative tasks.</p>
           </Timeline.Body>
-          <Button color="gray">
-            Learn More
-            <HiArrowNarrowRight className="ml-2 h-3 w-3" />
-          </Button>
+  
         </Timeline.Content>
       </Timeline.Item>
       <Timeline.Item>
         <Timeline.Point icon={HiCalendar} />
         <Timeline.Content>
-          <Timeline.Time>March 2022</Timeline.Time>
-          <Timeline.Title>Marketing UI design in Figma</Timeline.Title>
+          <Timeline.Time>Research Congress 2024</Timeline.Time>
+          <Timeline.Title>3rd Place Best in Layout Poster</Timeline.Title>
           <Timeline.Body>
-            All of the pages and components are first designed in Figma and we keep a parity between the two versions
-            even as we update the project.
+           <p className='dark:text-white'>Designed and developed a layout recognized for creativity and effectiveness.</p>
           </Timeline.Body>
         </Timeline.Content>
       </Timeline.Item>
       <Timeline.Item>
         <Timeline.Point icon={HiCalendar} />
         <Timeline.Content>
-          <Timeline.Time>April 2022</Timeline.Time>
-          <Timeline.Title>E-Commerce UI code in Tailwind CSS</Timeline.Title>
+          <Timeline.Time>Filipino during DSPC December 2023</Timeline.Time>
+          <Timeline.Title>2nd Place Online Publishing Feature Writer</Timeline.Title>
           <Timeline.Body>
-            Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
+            <p className='dark:text-white'>Awarded for exceptional feature writing skills in the online publishing category.</p>
+          </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point icon={HiCalendar} />
+        <Timeline.Content>
+          <Timeline.Time>6th Place Science Technology Writer</Timeline.Time>
+          <Timeline.Title>Filipino during DSPC April 2023</Timeline.Title>
+          <Timeline.Body>
+            <p className='dark:text-white'>Recognized for excellence in science and technology writing.</p>
           </Timeline.Body>
         </Timeline.Content>
       </Timeline.Item>
     </Timeline>
+        </div>
+        <div className="image-exp">
+          <Lottie animationData={animationData}/>
         </div>
     </div>
     </section>
